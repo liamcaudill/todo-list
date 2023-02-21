ede26583d19891ecde3a7a3e47e14dffb96b1b16
liamcaudill <liam.caudill@gmail.com>
2023-02-21 03:23:38 +0000

    added serve-static to serve index.html via nest

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,13 +5,10 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
+  /*
   @Get("/liam")
   getLiam() {
     return {time: new Date()}
   }
+  */
 }
