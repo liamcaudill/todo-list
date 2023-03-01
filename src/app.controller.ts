f8e52032be56d5621eaf7037f657e83613712cf6
liamcaudill <33670173+liamcaudill@users.noreply.github.com>
2023-03-01 17:09:23 -0500

    Setting up the frontend (#2)
    * Setting up the frontend
    
    * added serve-static to serve index.html via nest
    
    * Readded POST functionality, refactored todo data
    
    * List rendering tied to internal data, integrated add function
    
    * Fixed list update, added delete

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TodoDTO } from './todo/todo.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  constructor(private readonly appService: AppService) { }
+  /*
   @Get()
   getHello(): string {
     return this.appService.getHello();
@@ -14,10 +15,30 @@ export class AppController {
   getLiam() {
     return {time: new Date()}
   }
+ */
 
+  @Get("/api/todo")
+  getTodos(): TodoDTO[] {
+    return this.appService.items
+  }
+  
   @Post("/api/todo")
   createTodo(@Req() req) {
     const body = req.body;
     return this.appService.createTodo(body)
+  }
+  
+@Put("/api/todo/:id")
+  updateTodo(@Req() req, @Param() params) {
+
+    const idNum = parseInt(params.id)
+    const body = req.body
+    return this.appService.updateTodo(idNum, body)
+  }
+
+  @Delete("/api/todo/:id")
+  deleteTodo(@Param() params) {
+    const idNum = parseInt(params.id)
+    return this.appService.deleteTodo(idNum)
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,22 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { TodoDTO } from './todo/todo.dto';
 
 @Injectable()
 export class AppService {
-  public itemNum:number = 0
-  public items:string[] = []
+
+public itemNum: number = 0
+public items: TodoDTO[] = []
 
   getHello(): string {
     return 'Hello World!';
   }
 
   createTodo(createBody) {
-    //var decodedItem = JSON.parse(createBody)
-    var item:string = createBody.item
-    var itemNum = this.itemNum
-    this.items[itemNum] = item
+
+    var item: TodoDTO = {
+      id: this.itemNum,
+      title: createBody.title,
+      status: createBody.status
+    }
+
+    this.items[this.itemNum] = item
+    //this used to be before, see if anything breaks
     this.itemNum++
-    console.log(`Item: ${item} Item Num: ${itemNum}`)
-    console.log({items:this.items})
-    return{itemNum, items:this.items}
+    console.log(this.items)
+    return item
+  }
+
+  updateTodo(id: number, createBody) {
+    let tempItem: TodoDTO
+    //console.log(createBody)
+    this.items.map(todo => {
+      if (todo) {
+        if (todo.id === id) {
+          if ("title" in createBody) {
+            tempItem = { id, title: createBody.title, status: createBody.status }
+          } else if (!("title" in createBody)) {
+            const tempTitle = todo.title
+            tempItem = { id, title: tempTitle, status: createBody.status }
+          }
+          //this used to have +1 in the [id]
+          this.items[id] = tempItem
+        }
+      }
+    })
+    console.log(this.items)
+    return this.items
+  }
+
+  deleteTodo(id: number) {
+    //console.log(id)
+    this.items[id] = null
+    console.log(this.items)
+    return this.items
+
   }
 }
