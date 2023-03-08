9ecdceeead6018cf6173db9ae1bd532cff208e78
liamcaudill <33670173+liamcaudill@users.noreply.github.com>
2023-03-08 16:06:13 -0500

    Added redis, postgre, and prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { CacheModule} from '@nestjs/common';
+import * as redisStore from 'cache-manager-redis-store';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..'),
+    }),
+    ConfigModule.forRoot(),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
     }),
 
   ],
