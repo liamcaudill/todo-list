83f5d40d402a0650750c1eeb762dc8f206e5fc22
liamcaudill <33670173+liamcaudill@users.noreply.github.com>
2023-03-09 19:26:05 -0500

    removed redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { CacheModule } from '@nestjs/common';
-import * as redisStore from 'cache-manager-redis-store';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma.service';
 
@@ -15,13 +13,7 @@ import { PrismaService } from './prisma.service';
       rootPath: join(__dirname, '..'),
     }),
     ConfigModule.forRoot(),
-    CacheModule.register({
-      isGlobal: true,
-      // @ts-ignore
-      store: redisStore,
-      host: process.env.REDIS_HOST,
-      port: process.env.REDIS_PORT,
-    }),
+    
 
   ],
   controllers: [AppController],
